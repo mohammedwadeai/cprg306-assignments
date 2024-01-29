fdda902cd1dfaa9de2d2a984581a6d73b3119dd3
mohammedwadeai <mohammed.al-wadeai@edu.sait.ca>
2024-01-29 13:19:52 -0700

    WebDev2
    My Assignment 2

---
+++ app/page.js
@@ -0,0 +1,18 @@
+import Image from "next/image";
+import Link from "next/link";
+
+export default function Home() {
+  return (
+    <main className="flex min-h-screen flex-col items-center justify-between p-24">
+      <div className="z-10 max-w-5xl w-full items-center justify-between font-mono text-sm lg:flex">
+      <h1>CPRG 306: Web Development 2 - Assignments</h1>
+      <p>click <Link className="text-blue" href='/week2'>HERE</Link> for my info</p>
+       
+      </div>
+
+      
+
+   
+    </main>
+  );
+}
---
+++ app/week2/student-info.js
@@ -0,0 +1,12 @@
+import Link from "next/link";
+export default function StudentInfo() {
+    return (
+      <main>
+        <div>
+          <p>Mohammed alwadeai</p>
+          <p>My Github is : <Link className="text-blue-900 font-bold"  href="https://github.com/mohammedwadeai">mohammedwadeai</Link></p>
+        </div>
+      </main>
+    );
+  }
+  
