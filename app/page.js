3539647a7f2d5996bcb8b6f6ea1e36d97f58f261
mohammedwadeai <mohammed.al-wadeai@edu.sait.ca>
2024-02-05 22:41:08 -0700

    dev

--- app/page.js
+++ app/page.js
@@ -1,18 +1,23 @@
-import Image from "next/image";
-import Link from "next/link";
+import Link from 'next/link';
 
 export default function Home() {
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <div className="z-10 max-w-5xl w-full items-center justify-between font-mono text-sm lg:flex">
-      <h1>CPRG 306: Web Development 2 - Assignments</h1>
-      <p>click <Link className="text-blue" href='/week-2'>HERE</Link> for my info</p>
-      <p>click <Link className="text-blue" href='/week-3'>HERE</Link> for Shopping list</p>
+    <main className="flex min-h-screen flex-col items-center justify-center p-6 bg-gray-100">
+      <div className="text-center space-y-6">
+        <h1 className="text-4xl font-bold text-gray-900">CPRG 306: Web Development 2 - Assignments</h1>
+        <div className="space-y-4">
+          <p>
+            <Link className="text-lg text-blue-500 hover:text-blue-600 transition duration-150 ease-in-out" href="/week-2">
+              Click HERE for my info
+            </Link>
+          </p>
+          <p>
+            <Link className="text-lg text-blue-500 hover:text-blue-600 transition duration-150 ease-in-out" href="/week-3">
+              Click HERE for Shopping list
+            </Link>
+          </p>
+        </div>
       </div>
-
-      
-
-   
     </main>
   );
 }
