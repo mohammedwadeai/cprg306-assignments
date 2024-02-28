1e60ee326b9572eaf9c3e144686a2d232dc12ae4
mohammedwadeai <mohammed.al-wadeai@edu.sait.ca>
2024-02-28 10:18:18 -0700

    update dark-display

--- app/week-4/new-item.js
+++ app/week-4/new-item.js
@@ -24,15 +24,15 @@ export default function NewItem() {
       <div className="space-y-4">
         <div>
           <label htmlFor="name" className="text-white">Name</label>
-          <input type="text" id="name" value={name} onChange={(e) => setName(e.target.value)} required className="mt-1 block w-full p-2 rounded-md focus:ring focus:ring-opacity-50 focus:ring-offset-2 shadow-lg transition-all duration-300 ease-out hover:scale-110" />
+          <input type="text" id="name" value={name} onChange={(e) => setName(e.target.value)} required className="mt-1 block w-full p-2 rounded-md focus:ring focus:ring-opacity-50 focus:ring-offset-2 shadow-lg transition-all duration-300 ease-out hover:scale-110 text-gray-600" />
         </div>
         <div>
           <label htmlFor="quantity" className="text-white">Quantity</label>
-          <input type="number" id="quantity" min="1" max="99" value={quantity} onChange={(e) => setQuantity(Number(e.target.value))} required className="mt-1 block w-full p-2 rounded-md focus:ring focus:ring-opacity-50 focus:ring-offset-2 shadow-lg transition-all duration-300 ease-out hover:scale-110" />
+          <input type="number" id="quantity" min="1" max="99" value={quantity} onChange={(e) => setQuantity(Number(e.target.value))} required className="mt-1 block w-full p-2 rounded-md focus:ring focus:ring-opacity-50 focus:ring-offset-2 shadow-lg transition-all duration-300 ease-out hover:scale-110 text-gray-600 " />
         </div>
         <div>
           <label htmlFor="category" className="text-white ">Category</label>
-          <select id="category" value={category} onChange={(e) => setCategory(e.target.value)} className="mt-1 block w-full p-2 rounded-md focus:ring focus:ring-opacity-50 focus:ring-offset-2 shadow-lg transition-all duration-300 ease-out hover:scale-110">
+          <select id="category" value={category} onChange={(e) => setCategory(e.target.value)} className="mt-1 block w-full p-2 rounded-md focus:ring focus:ring-opacity-50 focus:ring-offset-2 shadow-lg transition-all duration-300 ease-out hover:scale-110 text-gray-600">
             <option value="produce">Produce</option>
             <option value="dairy">Dairy</option>
             <option value="bakery">Bakery</option>
